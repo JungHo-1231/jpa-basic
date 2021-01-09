9289fca86e1f4a41ffb9e68b21597b5c2b270cf5
JungHo <jungpark007@naver.com>
2021-01-10 03:56:54 +0900

    연관관계 기초

---
+++ jpa-basic/src/main/java/hellojap/JpaMain.java
@@ -0,0 +1,52 @@
+package hellojap;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            // 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+//            member.changeTeam(team);
+//            team.getMembers().add(member);
+            em.persist(member);
+
+            team.addMember(member);
+
+            em.flush();
+            em.clear();
+
+            Team findTeam = em.find(Team.class, team.getId());
+            List<Member> members = findTeam.getMembers();
+
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        } finally{
+            em.close();
+        }
+        // close
+        emf.close();
+    }
+}
---
+++ jpa-basic/src/main/java/hellojap/Member.java
@@ -0,0 +1,52 @@
+package hellojap;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
